9672ffaf9f40a92b673fc1d7ea925c85175ab439
Eric Umeohabike <eric2umeh@yahoo.com>
2025-07-14 14:26:42 +0100

    ingest route

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,10 @@
 import { inngest } from "@/app/utils/inngest/client";
-import { handleJobExpiration } from "@/app/utils/inngest/functions";
+import { handleJobExpiration, sendPeriodicJobListinngs } from "@/app/utils/inngest/functions";
 import { serve } from "inngest/next";
+import { send } from "process";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [handleJobExpiration],
+  functions: [handleJobExpiration, sendPeriodicJobListinngs],
 });

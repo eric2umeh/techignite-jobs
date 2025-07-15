ab9b8ceabfbbaa24b9fd51bed0e54476bb04fc81
Eric Umeohabike <eric2umeh@yahoo.com>
2025-07-15 14:03:03 +0100

    auth adjust

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,10 +1,14 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "./db";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./db"; // make sure this exports a valid Prisma client
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub, Google],
+  providers: [
+    GitHub,
+    Google,
+  ],
+  secret: process.env.AUTH_SECRET,
 });

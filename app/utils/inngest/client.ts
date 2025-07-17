2ff86e0718bdfeb7f3245a329b09d2eb1708dc06
Eric Umeohabike <eric2umeh@yahoo.com>
2025-07-17 13:05:42 +0100

    add secret inngest key

--- app/utils/inngest/client.ts
+++ app/utils/inngest/client.ts
@@ -1,4 +1,10 @@
-import { Inngest } from "inngest";
+import { Inngest } from "inngest"
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "techignite-jobs" });
+export const inngest = new Inngest({
+  id: "techignite-jobs",
+  // Only use event key in production
+  ...(process.env.NODE_ENV === "production" && {
+    eventKey: process.env.INNGEST_EVENT_KEY,
+  }),
+})
